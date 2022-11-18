78bca16027ecbb695174743be37744b6400c5950
tangcy03 <tangcy03@outlook.com>
2022-11-18 15:45:21 +0800

    add python defines

--- main_dct.cpp
+++ main_dct.cpp
@@ -1,9 +1,10 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 #include <assert.h>
 #include <iostream>
 #include <iomanip>
 #include <vector>
 #include <string>
-#include <Python.h>
 
 #pragma pack(1)
 
